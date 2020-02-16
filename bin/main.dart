7c9631fa3de31d94225934483b91f564960fb873
Daniel Carvalho <daniel.devop@gmail.com>
2020-02-16 06:56:37 -0300

    removendo speed do construtor e criando um get

--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,17 @@
 class Bicycle {
   int cadence;
-  int speed;
+  int _speed = 0;
   int gear;
 
-  Bicycle(this.cadence, this.speed, this.gear);
+  Bicycle(this.cadence, this.gear);
 
   @override
-  String toString() => 'Bicycle: $speed mph';
+  String toString() => 'Bicycle: $_speed mph';
+
+  int get speed => _speed;
 }
 
 void main() {
-  var bike = Bicycle(2, 0, 1);
+  var bike = Bicycle(2, 1);
   print(bike);
 }
