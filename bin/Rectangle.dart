86ec8fa1aa523e24b31e3c1c8d035d16cdad32a9
Daniel Carvalho <daniel.devop@gmail.com>
2020-02-17 22:53:05 -0300

    usando o construtor com parâmetros opcionais

--- bin/Rectangle.dart
+++ bin/Rectangle.dart
@@ -12,4 +12,9 @@ class Rectangle {
       'Origin (${origin.x}, ${origin.y}), width: $width, height: $height';
 }
 
-main() {}
+main() {
+  print(Rectangle(origin: const Point(10, 20), width: 100, height: 200));
+  print(Rectangle(origin: const Point(10, 10)));
+  print(Rectangle(width: 100));
+  print(Rectangle());
+}
