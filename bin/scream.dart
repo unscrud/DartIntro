ed95456378b6519bdf3abf491ef13bec53cae583
Daniel Carvalho <daniel.devop@gmail.com>
2020-02-23 20:41:11 -0300

    Use Dart for functional programming

--- bin/scream.dart
+++ bin/scream.dart
@@ -2,8 +2,15 @@ String scream(int length) => "A${'a' * length}h!";
 
 main() {
   final values = [1, 2, 3, 5, 10, 50];
+
+//  imperativo
 //  for (var length in values) {
 //    print(scream(length));
 //  }
-  values.map(scream).forEach(print);
+
+//  funcional
+//  values.map(scream).forEach(print);
+
+//  iteravel
+  values.skip(1).take(3).map(scream).forEach(print);
 }
